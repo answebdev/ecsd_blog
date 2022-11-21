3c359aee5ae5ba8e96714ebb06f5b228afe17430
ANS <36783010+answebdev@users.noreply.github.com>
2022-11-20 16:48:26 -0800

    add shell components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+// import Navbar from './components/layout/Navbar';
+import Landing from './components/pages/Landing';
+import Home from './components/pages/Home';
+import Error from './components/pages/Error';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route exact path='/' element={<Landing />} />
+        <Route exact path='/home' element={<Home />} />
+        {/* <Route exact path='/:slug' element={<Course />} /> */}
+        <Route path='*' element={<Error />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
