f06c13744a42c2ac7cc7c44d9c0e328d8b0495cb
ANS <36783010+answebdev@users.noreply.github.com>
2022-11-26 16:08:04 -0800

    update Error Page styles

--- src/components/pages/Error.js
+++ src/components/pages/Error.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import logo from '../../logo.svg';
+import robot from '../../broken-robot.png';
 import classes from '../../styles/Error.module.css';
+
+// Robot image: https://icons8.com/icon/JB5lRIhzKC0y/broken-robot
 
 const Error = () => {
   return (
@@ -21,7 +23,7 @@ const Error = () => {
       </Helmet>
       <div className={classes.App}>
         <header className={classes.AppHeader}>
-          <img src={logo} className={classes.AppLogo} alt='logo' />
+          <img src={robot} className={classes.AppLogo} alt='logo' />
           <p>
             <code>Oops! Page Not Found.</code>
           </p>
