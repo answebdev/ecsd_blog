bc52a8ccb9e3d003fcc1da9b82d9f9ab9117ddae
ANS <36783010+answebdev@users.noreply.github.com>
2022-11-28 19:45:25 -0800

    update navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,41 +1,181 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import classes from '../../styles/Navbar.module.css';
 
-const Navbar = () => {
+const drawerWidth = 240;
+
+const Navbar = (props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant='h6' sx={{ my: 2 }}>
+        <Link style={{ textDecoration: 'none', color: '#1a2027' }} to='/'>
+          LOW CORE
+        </Link>
+      </Typography>
+      <Divider />
+      <List style={{ textAlign: 'center' }} component='nav'>
+        <Link style={{ textDecoration: 'none', color: '#1a2027' }} to='/home'>
+          <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemText primary='Home' />
+          </ListItemButton>
+        </Link>
+        <Link style={{ textDecoration: 'none', color: '#1a2027' }} to='/blog'>
+          <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemText primary='Blog' />
+          </ListItemButton>
+        </Link>
+        <Link
+          style={{ textDecoration: 'none', color: '#1a2027' }}
+          to='/resources'
+        >
+          <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemText primary='Resources' />
+          </ListItemButton>
+        </Link>
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
     <div className='navbar'>
-      <Box sx={{ flexGrow: 1 }}>
+      {/* For fixed/static navbar, use this opening <Box> tag code: */}
+      {/* <Box sx={{ flexGrow: 1 }}>
         <AppBar
+          component='nav'
           position='static'
+          sx={{
+            backgroundColor: '#545454',
+          }}
+        > */}
+      <Box sx={{ display: 'flex' }}>
+        <AppBar
+          component='nav'
           sx={{
             backgroundColor: '#545454',
           }}
         >
           <Toolbar>
-            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            <IconButton
+              color='inherit'
+              aria-label='open drawer'
+              edge='start'
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant='h6'
+              component='div'
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
               <Link className={classes.TitleLink} to='/'>
                 LOW CORE
               </Link>
             </Typography>
-            <Link className={classes.Link} to='/home' color='inherit'>
-              Home
-            </Link>
-            <Link className={classes.Link} to='/blog' color='inherit'>
-              Blog
-            </Link>
-            <Link className={classes.Link} to='/resources' color='inherit'>
-              Resources
-            </Link>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              <Link className={classes.Link} to='/home' color='inherit'>
+                Home
+              </Link>
+              <Link className={classes.Link} to='/blog' color='inherit'>
+                Blog
+              </Link>
+              <Link className={classes.Link} to='/resources' color='inherit'>
+                Resources
+              </Link>
+            </Box>
           </Toolbar>
         </AppBar>
+        <Box component='nav'>
+          <Drawer
+            container={container}
+            variant='temporary'
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box component='main'>
+          <Toolbar />
+        </Box>
       </Box>
     </div>
   );
 };
 
 export default Navbar;
+
+// ORIGINAL NAVBAR CODE:
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import classes from '../../styles/Navbar.module.css';
+
+// const Navbar = () => {
+//   return (
+//     <div className='navbar'>
+//       <Box sx={{ flexGrow: 1 }}>
+//         <AppBar
+//           position='static'
+//           sx={{
+//             backgroundColor: '#545454',
+//           }}
+//         >
+//           <Toolbar>
+//             <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+//               <Link className={classes.TitleLink} to='/'>
+//                 LOW CORE
+//               </Link>
+//             </Typography>
+//             <Link className={classes.Link} to='/home' color='inherit'>
+//               Home
+//             </Link>
+//             <Link className={classes.Link} to='/blog' color='inherit'>
+//               Blog
+//             </Link>
+//             <Link className={classes.Link} to='/resources' color='inherit'>
+//               Resources
+//             </Link>
+//           </Toolbar>
+//         </AppBar>
+//       </Box>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
