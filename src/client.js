4321d869303d9b1c6058c1c2d608332649eab6f6
ANS <36783010+answebdev@users.noreply.github.com>
2022-11-30 17:22:39 -0800

    remove environment variable

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export default sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID, // find this at manage.sanity.io or in your sanity.json
+  projectId: 'gw9yxpul', // find this at manage.sanity.io or in your sanity.json
   dataset: 'production', // this is from those question during 'sanity init'
   apiVersion: '2022-11-25',
   useCdn: true,
