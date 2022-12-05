5dc3d3b0b113008a25938414cf8bf823ac10bef1
ANS <36783010+answebdev@users.noreply.github.com>
2022-12-04 16:04:42 -0800

    add CSS module

--- src/components/pages/OnePost.js
+++ src/components/pages/OnePost.js
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { format } from 'date-fns';
-import { useParams } from 'react-router-dom';
 import sanityClient from '../../client.js';
 import BlockContent from '@sanity/block-content-to-react';
 import imageUrlBuilder from '@sanity/image-url';
-
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-
 import Spinner from '../misc/Spinner.js';
+import classes from '../../styles/OnePost.module.css';
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -53,72 +52,54 @@ const Blog = () => {
         <title>Low Core | Blog | {postData.title}</title>
       </Helmet>
       <div
+        className={classes.HeroImage}
         style={{
-          marginTop: '-70px',
-          zIndex: '-10',
-          position: 'relative',
-          height: '500px',
-          objectFit: 'contain',
-          overFlow: 'hidden',
           backgroundImage: `linear-gradient( 0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${urlFor(
             postData.mainImage
           ).url()})`,
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
         }}
-        className='hero-image'
       >
-        <div
-          style={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%,-50%)',
-          }}
-          className='container'
-        >
-          <h1 style={{ fontsize: '72px', color: '#ffffff' }}>
-            <span style={{ padding: '6px 14px', display: 'inline-block' }}>
+        <div className={classes.HeroContainer}>
+          <h1
+            className={classes.HeroHeader}
+            //style={{ fontsize: '72px', color: '#ffffff' }}
+          >
+            <span
+              className={classes.HeroHeaderSpan}
+              //style={{ padding: '6px 14px', display: 'inline-block' }}
+            >
               {postData.title}
             </span>
           </h1>
         </div>
       </div>
-      <div style={{ padding: '2em 4em 0 4em' }}>
-        <h2
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          {postData.title}
-        </h2>
+      <div className={classes.AvatarContainer}>
+        <h2 className={classes.AvatarH2}>{postData.title}</h2>
         <div>
-          <p style={{ textAlign: 'center', lineHeight: '2' }}>
-            By {postData.author}
-          </p>
-
+          <p className={classes.AvatarAuthorText}>By {postData.author}</p>
+          <br />
           <Stack
-            style={{ display: 'flex', justifyContent: 'center' }}
+            sx={{ display: 'flex', justifyContent: 'center' }}
             direction='row'
             spacing={2}
           >
             {postData.studentImage && (
               <Avatar
-                alt={postData.author}
                 sx={{ width: 100, height: 100 }}
                 src={urlFor(postData.studentImage).url()}
+                alt={postData.author}
               />
             )}
           </Stack>
         </div>
       </div>
 
-      <div style={{ padding: '4em' }}>
+      <div className={classes.MainTextContainer}>
         <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
           {format(new Date(postData.publishedAt), 'MMMM dd, yyyy')}
         </Typography>
-        <div style={{ fontSize: '20px', lineHeight: '1.6' }}>
+
+        <div className={classes.MainText}>
           <BlockContent
             blocks={postData.body}
             projectId={sanityClient.clientConfig.projectId}
@@ -127,7 +108,9 @@ const Blog = () => {
         </div>
 
         <div>
-          <Link to='/blog'>Back</Link>
+          <Link style={{ textDecoration: 'none' }} to='/blog'>
+            Back to Blog
+          </Link>
         </div>
       </div>
     </div>
