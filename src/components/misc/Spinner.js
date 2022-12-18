710805edef65662bc98c900a7edcaa19c3ae654e
ANS <36783010+answebdev@users.noreply.github.com>
2022-12-17 18:09:54 -0800

    refactor code

--- src/components/misc/Spinner.js
+++ src/components/misc/Spinner.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import spinner from '../../assets/img/spinner.gif';
+import classes from '../../styles/Spinner.module.css';
 
 const Spinner = () => (
   <img
+    className={classes.Spinner}
     src={spinner}
     alt='Loading...'
     // style={{ width: '35px', height: 'auto', margin: 'auto', display: 'block' }}
-    style={{
-      width: '35px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      position: 'absolute',
-      margin: 'auto',
-      top: '0',
-      left: '0',
-      right: '0',
-      bottom: '0',
-    }}
   />
 );
 
